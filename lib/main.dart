8c8ffec4c76028e80d4fefa87e00f7c0860988ea
Alexies Iglesia <ialexies@gmail.com>
2022-05-07 01:46:52 +0800

    Update UI, first commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import 'package:get/get.dart';
+
+import 'app/routes/app_pages.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(
+    ScreenUtilInit(
+      designSize: const Size(360, 800),
+      child: GetMaterialApp(
+        title: "Application",
+        initialRoute: AppPages.INITIAL,
+        getPages: AppPages.routes,
+      ),
+    ),
+  );
+}
